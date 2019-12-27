30bd1f7d45dc9b365a9943bc79c008029f9c667a
kazuki13070311 <kazuki13070311@gmail.com>
2019-12-27 20:10:20 +0900

    新規作成機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,26 +1,40 @@
 # frozen_string_literal: true
 
 class Admin::UsersController < ApplicationController
+  before_action :require_admin, only: [:index]
+  skip_before_action :login_required, if: proc{action_name == "new" || action_name == "create"}
+  
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    if current_user.admin
+      @user = User.find(params[:id])
+    else
+      @user = User.find(current_user.id)
+    end
   end
 
   def new
     @user = User.new
+    @admin = current_user.admin if current_user
   end
 
   def edit
-    @user = User.find(params[:id])
+    if current_user.admin
+      @user = User.find(params[:id])
+    else
+      @user = User.find(current_user.id)
+    end
+    @admin = current_user.admin if current_user
   end
 
   def create
     @user = User.new(user_params)
-
+    #@user.active = false
     if @user.save
+      session[:user_id] = @user.id unless current_user
       redirect_to admin_user_url(@user), notice: "ガーディアン「#{@user.name}を登録しました。」"
     else
       render :new
@@ -29,7 +43,6 @@ class Admin::UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-
     if @user.update(user_params)
       redirect_to admin_user_url(@user), notice: "ガーディアン「#{@user.name}」を更新しました。"
     else
@@ -50,6 +63,10 @@ class Admin::UsersController < ApplicationController
   end
 
   def require_admin
-    redirect_to root_path unless current_user.admin?
+    if current_user
+      redirect_to root_path unless current_user.admin?
+    else
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/new'
+  get '/signup', to: 'admin/users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
